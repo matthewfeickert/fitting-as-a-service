1089f1129835c61b9f7f99e0d04275e2372f4c93
Matthew Feickert <matthew.feickert@cern.ch>
2021-02-24 23:30:36 -0600

    seperate into functions

--- demo_fit.py
+++ demo_fit.py
@@ -1,94 +1,115 @@
+import json
 import sys
 from time import sleep
-import json
-NUM_RUNS = 70
+
 import requests
 from funcx.sdk.client import FuncXClient
 from pyhf.contrib.utils import download
 
-pyhf_endpoint = 'a727e996-7836-4bec-9fa2-44ebf7ca5302'
-
-fxc = FuncXClient()
-fxc.max_requests = 200
 
 def prepare_workspace(data):
     import pyhf
-    w = pyhf.Workspace(data)
-    return w
 
-prepare_func = fxc.register_function(prepare_workspace)
+    return pyhf.Workspace(data)
 
 
-def infer_hypotest(w, metadata, doc):
-    import pyhf
+def infer_hypotest(workspace, metadata, doc):
     import time
 
+    import pyhf
+
     tick = time.time()
-    m = w.model(
+    model = workspace.model(
         patches=[doc],
         modifier_settings={
             "normsys": {"interpcode": "code4"},
             "histosys": {"interpcode": "code4p"},
-        }
+        },
     )
-    d = w.data(m)
+    data = workspace.data(model)
+    test_poi = 1.0
     return {
-        'metadata': metadata,
-        'CLs_obs': float(pyhf.infer.hypotest(1.0, d, m, test_stat="qtilde")),
-        'Fit-Time': time.time() - tick
+        "metadata": metadata,
+        "CLs_obs": float(
+            pyhf.infer.hypotest(test_poi, data, model, test_stat="qtilde")
+        ),
+        "Fit-Time": time.time() - tick,
     }
 
 
-infer_func = fxc.register_function(infer_hypotest)
-
-# locally get pyhf pallet for analysis
-download("https://doi.org/10.17182/hepdata.90607.v3/r3", "1Lbb-pallet")
-with open("1Lbb-pallet/BkgOnly.json") as bkgonly_json:
-    bkgonly_workspace = json.load(bkgonly_json)
-
-prepare_task = fxc.run(bkgonly_workspace, endpoint_id=pyhf_endpoint, function_id=prepare_func)
-
-# While this cooks, let's read in the patch set
-patches = None
-with open("1Lbb-pallet/patchset.json") as f:
-    patches = json.load(f)
-patch = patches['patches'][0]
-name = patch['metadata']['name']
-
-w = None
-
-while not w:
-    try:
-        w = fxc.get_result(prepare_task)
-    except Exception as e:
-        print("prepare ", e)
-        sleep(15)
-
-print("--------------------")
-print(w)
-
-tasks = {}
-for i in range(NUM_RUNS):
-    patch = patches['patches'][i]
-    name = patch['metadata']['name']
-    task_id = fxc.run(w, patch['metadata'], patch['patch'], endpoint_id=pyhf_endpoint, function_id=infer_func)
-    tasks[name] = {"id": task_id, "result": None}
-
-
 def count_complete(l):
-    return len(list(filter(lambda e: e['result'], l)))
+    return len(list(filter(lambda e: e["result"], l)))
 
 
-while count_complete(tasks.values()) < NUM_RUNS:
-    for task in tasks.keys():
-        if not tasks[task]['result']:
-            try:
-                result = fxc.get_result(tasks[task]['id'])
-                print(f"Task {task} complete, there are {count_complete(tasks.values())} results now")
-                tasks[task]['result'] = result
-            except Exception as e:
-                print(e)
-                sleep(15)
+def main():
+    NUM_RUNS = 70
+    pyhf_endpoint = "a727e996-7836-4bec-9fa2-44ebf7ca5302"
 
-print("--------------------")
-print(tasks.values())
+    fxc = FuncXClient()
+    fxc.max_requests = 200
+
+    prepare_func = fxc.register_function(prepare_workspace)
+
+    infer_func = fxc.register_function(infer_hypotest)
+
+    # locally get pyhf pallet for analysis
+    download("https://doi.org/10.17182/hepdata.90607.v3/r3", "1Lbb-pallet")
+    with open("1Lbb-pallet/BkgOnly.json") as bkgonly_json:
+        bkgonly_workspace = json.load(bkgonly_json)
+
+    prepare_task = fxc.run(
+        bkgonly_workspace, endpoint_id=pyhf_endpoint, function_id=prepare_func
+    )
+
+    # While this cooks, let's read in the patch set
+    patches = None
+    with open("1Lbb-pallet/patchset.json") as f:
+        patches = json.load(f)
+    patch = patches["patches"][0]
+    name = patch["metadata"]["name"]
+
+    w = None
+
+    while not w:
+        try:
+            w = fxc.get_result(prepare_task)
+        except Exception as e:
+            print("prepare ", e)
+            sleep(15)
+
+    print("--------------------")
+    print(w)
+
+    NUM_RUNS = len(patches["patches"])
+    tasks = {}
+    for i in range(NUM_RUNS):
+        patch = patches["patches"][i]
+        name = patch["metadata"]["name"]
+        task_id = fxc.run(
+            w,
+            patch["metadata"],
+            patch["patch"],
+            endpoint_id=pyhf_endpoint,
+            function_id=infer_func,
+        )
+        tasks[name] = {"id": task_id, "result": None}
+
+    while count_complete(tasks.values()) < NUM_RUNS:
+        for task in tasks.keys():
+            if not tasks[task]["result"]:
+                try:
+                    result = fxc.get_result(tasks[task]["id"])
+                    print(
+                        f"Task {task} complete, there are {count_complete(tasks.values())} results now"
+                    )
+                    tasks[task]["result"] = result
+                except Exception as e:
+                    print(e)
+                    sleep(15)
+
+    print("--------------------")
+    print(tasks.values())
+
+
+if __name__ == "__main__":
+    main()
